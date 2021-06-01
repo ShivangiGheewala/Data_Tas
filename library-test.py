6213f8b9022c501720ec37bdc687ab168ee4ed07
Shivangi Gheewala <shivangigheewala08@gmail.com>
2021-06-01 11:54:17 +1000

    create library-testfile

---
+++ library-test.py
@@ -0,0 +1,15 @@
+import tensorflow as tf
+from tensorflow import keras
+import matplotlib as plot
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import sklearn as skl
+
+print("tensorflow: %s" % tf.__version__)
+print("keras: %s" % keras.__version__)
+print("numpy: %s" % np.__version__)
+print("pandas: %s" % pd.__version__)
+print("matplotlib: %s" % plot.__version__)
+print("seaborn: %s" % sns.__version__)
+print("sklearn: %s" % skl.__version__)
